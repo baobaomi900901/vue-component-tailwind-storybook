f929c7323a87b9ff4d789608e0ea10fb171210a3
zixu <57753561+Zixuip@users.noreply.github.com>
2022-02-12 19:56:59 +0800

    update tailwind.config.js & add .gitignore file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-	content: ["./src/**/*.{html,js}"],
+	purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+	darkMode: false, // or 'media' or 'class'
 	theme: {
 		extend: {},
 	},
+	variants: {
+		extend: {},
+	},
 	plugins: [],
-};
+}
